c7dbe3f5210366268544ddd1ba0bda4a1e9e78a1
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-05-03 20:46:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,14 +25,27 @@ class Song
     @@genres.uniq
   end
   
-  def genre_count
-    count = {}
-		@@genres.each do |genre|
-			count[genre] = @@genres.count(genre)
-		end
-		count
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre_name|
+      if genre_count[genre_name]
+        genre_count[genre_name] += 1
+      else
+        genre_count[genre_name] = 1
+      end
+    end
+    genre_count
   end
   
-  def artist_count
+  def self.artist_count
+        artist_count = {}
+    @@artists.each do |artist_name|
+      if artist_count[artist_name]
+        artist_count[artist_name] += 1
+      else
+        artist_count[artist_name] = 1
+      end
+    end
+    artist_count
   end
 end
